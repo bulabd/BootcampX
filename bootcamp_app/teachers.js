5102f30db8a5f5ba2639df5ac18e66db762949ef
bulabd <bulabd@outlook.com>
2022-05-04 23:23:55 +0000

    Update queries to parameterized queries to prevent sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2] || 'JUL02';
+
+const values = [`%${cohortName}%`];
+
 pool.query(`
 select distinct teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
 from assistance_requests
 join teachers on teachers.id = teacher_id
 join students on students.id = student_id
 join cohorts on cohorts.id = cohort_id
-where cohorts.name = '${process.argv[2] || 'JUL02'}'
+where cohorts.name LIKE $1
 group by teachers.name, cohorts.name
 order by teachers.name;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
